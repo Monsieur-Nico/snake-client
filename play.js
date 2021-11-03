b7e5918fb1684539db1c6de18e0a10182e006ba8
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2021-11-03 20:22:19 +0000

    added client.js

--- play.js
+++ play.js
@@ -1,24 +1,5 @@
 const net = require("net");
 const { consumers } = require("stream");
+const connect = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on('connect', () => {
-    console.log("Connected to game server!");
-  });
-  conn.on('data', data => {
-    console.log(`data: ${data}`);
-  });
-  
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
+connect;
